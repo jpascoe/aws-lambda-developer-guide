62bbe5d35921fc0d7e3f2b33c773dcc0f3481817
Jason Pascoe <jpascoe@growthexponent.com>
2021-11-15 15:41:07 +0800

    fork repo

--- sample-apps/rds-mysql/5-create-dbtable.sh
+++ sample-apps/rds-mysql/5-create-dbtable.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eo pipefail
 FUNCTION=$(aws cloudformation describe-stack-resource --stack-name rds-mysql --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
-aws lambda invoke --function-name $FUNCTION --payload file://events/db-create-table.json out.json
+PAYLOAD=$(cat ./events/db-create-table.json | base64)
+aws lambda invoke --function-name $FUNCTION --payload $PAYLOAD out.json
--- sample-apps/rds-mysql/6-invoke.sh
+++ sample-apps/rds-mysql/6-invoke.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 FUNCTION=$(aws cloudformation describe-stack-resource --stack-name rds-mysql --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
-
+PAYLOAD=$(cat ./events/db-read-table.json | base64)
 while true; do
-  aws lambda invoke --function-name $FUNCTION --payload file://events/db-read-table.json out.json
+  aws lambda invoke --function-name $FUNCTION --payload $PAYLOAD out.json
   cat out.json
   echo ""
   sleep 2
